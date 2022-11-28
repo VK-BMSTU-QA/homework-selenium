d719e4cf0efa078271097bbbd5ead8396e6900ec
Kirill Katashinsky <kir20-01@ya.ru>
2022-11-29 00:13:14 +0300

    Added tests

--- hw/code/ui/components/login_page.py
+++ hw/code/ui/components/login_page.py
@@ -8,10 +8,10 @@ class LoginPage(BaseComponent):
     PATH = paths.LOGIN
 
     def login(self, phone, code):
-        phone_input = self.find(self.locators.PHONE_INPUT)
-        self.send_keys_elem(phone_input, phone)
+        self.send_keys(self.locators.PHONE_INPUT, phone)
         self.click((self.locators.LOGIN_BUTTON))
-        code_input = self.find(self.locators.CODE_INPUT)
-        self.send_keys_elem(code_input, code)
+
+        self.send_keys(self.locators.CODE_INPUT, code)
         self.click((self.locators.CONFIRM_CODE_BUTTON))
+
         assert self.is_visible(self.locators.PROFILE_BUTTON)
