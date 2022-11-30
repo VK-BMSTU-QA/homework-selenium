b4bf28f564d25cbc751d489ecd47bbef12fc4651
Victor Ivanov <vi.ivanov@vk.team>
2022-11-30 15:32:55 +0300

    add name

--- hw/code/tests/test_signup.py
+++ hw/code/tests/test_signup.py
@@ -10,30 +10,30 @@ errors = {
     "repeat_pass": "Введенные пароли не совпадают",
 }
 
-class SignUpTest(BaseTestCase):
+class SignUpTest(BaseTestCase): # Иванов Виктор Михайлович
     def setUp(self):
         super().setUp()
         self.page = SignUpPage(self.driver)
 
-    def test_signup(self):
+    def test_signup(self): # Иванов Виктор Михайлович
         self.page.open()
         self.page.signup(time.time(), authorization_data["password"], authorization_data["password"])
         Header.create(self.driver)
         self.assertEqual("https://planexa.ru/base", self.driver.current_url)
 
-    def test_signup_empty_error(self):
+    def test_signup_empty_error(self): # Иванов Виктор Михайлович
         self.page.open()
         self.page.btn_enter.click()
     
         self.assertEqual(errors["empty_enter"], self.page.get_error())
 
-    def test_signup_already_exist(self):
+    def test_signup_already_exist(self): # Иванов Виктор Михайлович
         self.page.open()
         self.page.signup(authorization_data["login"], authorization_data["password"], authorization_data["password"])
 
         self.assertEqual(errors["already_exist"], self.page.get_error())
 
-    def test_signup_repeat_pass(self):
+    def test_signup_repeat_pass(self): # Иванов Виктор Михайлович
         self.page.open()
         self.page.signup(time.time(), authorization_data["password"], authorization_data["password"] + "1")
 
