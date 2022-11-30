3c7432ef48f5494ca447d5a3482b4163cb58b5ba
alnaydenov <alnaydenov@ozon.ru>
2022-11-30 11:35:27 +0300

    Add author of login tests

--- hw/code/tests/test_login.py
+++ hw/code/tests/test_login.py
@@ -1,3 +1,4 @@
+# Найденов Александр Валентинович
 from pageobjects.pages.login import LoginPage
 from pageobjects.components.header import Header
 from tests.base_test_case import BaseTestCase
@@ -8,24 +9,24 @@ errors = {
     "wrong_user": "Пожалуйста, проверьте правильность написания логина и пароля",
 }
 
-class LoginTest(BaseTestCase):
+class LoginTest(BaseTestCase): # Найденов Александр Валентинович
     def setUp(self):
         super().setUp()
         self.page = LoginPage(self.driver)
 
-    def test_login_empty_error(self):
+    def test_login_empty_error(self): # Найденов Александр Валентинович
         self.page.open()
         self.page.btn_enter.click()
     
         self.assertEqual(errors["empty_enter"], self.page.get_error())
 
-    def test_login_wrong_user(self):
+    def test_login_wrong_user(self): # Найденов Александр Валентинович
         self.page.open()
         self.page.login(authorization_data["wrong_login"], authorization_data["wrong_password"])
 
         self.assertEqual(errors["wrong_user"], self.page.get_error())
     
-    def test_login(self):
+    def test_login(self): # Найденов Александр Валентинович
         self.page.open()
         self.page.login(authorization_data["login"], authorization_data["password"])
         Header.create(self.driver)
