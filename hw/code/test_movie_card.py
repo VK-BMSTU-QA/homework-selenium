a22dbe836c1a52de1afa1d843019d881097f3be9
iiivan-lemon <lipton2807@yandex.ru>
2022-12-01 00:16:56 +0300

    refactor

--- hw/code/test_movie_card.py
+++ hw/code/test_movie_card.py
@@ -6,23 +6,25 @@ from selenium.webdriver import ActionChains
 from hw.code.ui.pages.base_page import BasePage
 from hw.code.ui.base_case import BaseCase
 from hw.code.ui.pages.series_page import SeriesPage
-from hw.code.ui.pages.serial_page import SerialPage
+
 
 class TestLikeButton(BaseCase):
     authorize = True
     button_locator = BasePage.locators.BUTTON_LIKE
 
-
     # @pytest.mark.skip("SKIP")
 
     def test_like_switching(self):
         time.sleep(3)
-        a = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0].get_attribute("class").split(" ")
-        assert 'active-like' not in str(a)
+        a = self.base_page.wait_visability_of_elem(self.button_locator)
+
+        #  a = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0]
+        assert 'active-like' not in str(a.get_attribute("class").split(" "))
         self.base_page.click(self.button_locator, 10)
         time.sleep(3)
-        a = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0].get_attribute("class").split(" ")
-        assert 'active-like' in str(a)
+        a = self.base_page.wait_visability_of_elem(self.button_locator)
+        # a = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0].get_attribute("class").split(" ")
+        assert 'active-like' in str(a.get_attribute("class").split(" "))
         self.base_page.click(self.button_locator, 10)
 
 
@@ -30,11 +32,14 @@ class TestCard(BaseCase):
     authorize = True
     button_locator = BasePage.locators.CARD
     expected_url = "https://movie-space.ru/movie/"
+
     # @pytest.mark.skip("SKIP")
 
     def test_card_switching(self):
         time.sleep(3)
-        id_movie = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0].get_attribute("id")
+        id_movie = self.base_page.wait_visability_of_elem(self.button_locator).get_attribute(
+            "id")
+        # id_movie = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0]
         self.base_page.click(self.button_locator, 10)
         time.sleep(3)
         assert str(self.driver.current_url) == (self.expected_url + id_movie)
@@ -43,14 +48,16 @@ class TestCard(BaseCase):
 class TestCardHover(BaseCase):
     authorize = True
     button_locator = BasePage.locators.CARD
+
     # @pytest.mark.skip("SKIP")
 
     def test_card_hover(self):
         time.sleep(3)
-        element = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0]
+        element = self.base_page.wait_visability_of_elem(self.button_locator)
+        # element = self.base_page.driver.find_elements(self.button_locator[0], self.button_locator[1])[0]
         styles_before = {
-        prop: element.value_of_css_property(prop)
-        for prop in ['transform']
+            prop: element.value_of_css_property(prop)
+            for prop in ['transform']
         }
         ActionChains(self.driver).move_to_element(element).perform()
 
@@ -65,6 +72,7 @@ class TestEpisodeCard(BaseCase):
     authorize = True
     element_locator = SeriesPage.locators.EPISODE_CARD
     expected_url = "https://movie-space.ru/player/"
+
     # @pytest.mark.skip("SKIP")
 
     def test_page_switching(self):
@@ -75,7 +83,3 @@ class TestEpisodeCard(BaseCase):
         self.base_page.click(self.element_locator, 10)
         assert str(self.driver.current_url) == (self.expected_url + id_movie + "?seas=1&ep=1")
         time.sleep(1)
-
-
-
-
--- hw/code/test_movie_selection.py
+++ hw/code/test_movie_selection.py
@@ -16,9 +16,11 @@ class TestArrowsClick(BaseCase):
 
     def test_arrows_clicking(self):
         time.sleep(3)
-        selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
+        selection = self.base_page.wait_visability_of_elem(BasePage.locators.SELECTION)
+        # selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
         ActionChains(self.driver).move_to_element(selection).perform()
-        child_selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.CHILD_SELECTION[1])[0])
+        child_selection = self.base_page.wait_visability_of_elem(BasePage.locators.CHILD_SELECTION)
+       #  child_selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.CHILD_SELECTION[1])[0])
         for k in [self.button_right, self.button_left]:
             before_style = child_selection.value_of_css_property('transform')
             self.base_page.click(k, 10)
@@ -33,11 +35,13 @@ class TestArrowsShow(BaseCase):
 
     def test_arrows_show(self):
         time.sleep(3)
-        selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
+        selection = self.base_page.wait_visability_of_elem(BasePage.locators.SELECTION)
+        # selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
         self.base_page.click(self.button_right, 10)
-        time.sleep(1)
+
         ActionChains(self.driver).move_to_element(selection).perform()
         for button in [self.button_right, self.button_left]:
+            time.sleep(1)
             button = (self.base_page.driver.find_elements_by_xpath(button[1])[0])
             assert 'hover' in button.get_attribute("class")
             time.sleep(2)
@@ -50,7 +54,8 @@ class TestArrowsHidden(BaseCase):
 
     def test_arrows_unshow(self):
         time.sleep(3)
-        selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
+        selection = self.base_page.wait_visability_of_elem(BasePage.locators.SELECTION)
+        #selection = (self.base_page.driver.find_elements_by_xpath(BasePage.locators.SELECTION[1])[0])
         ActionChains(self.driver).move_to_element(selection).perform()
         button_left = (self.base_page.driver.find_elements_by_xpath(self.button_left[1])[0])
         button_right = (self.base_page.driver.find_elements_by_xpath(self.button_right[1])[0])
